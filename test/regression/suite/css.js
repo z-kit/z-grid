901eb8ba72eaf2d92337f2e3c4b04e916e13afca
Matheus Kautzmann <kautzmann5@gmail.com>
2017-01-25 21:41:58 -0200

    Add a test for every story example. Close #2

--- test/regression/suite/css.js
+++ test/regression/suite/css.js
@@ -6,4 +6,115 @@ module.exports = {
       .assert.containsText('.z-grid__col--12-12 > h1', '12-12')
       .end();
   },
+  gutter: (browser) => {
+    browser
+      .url('http://localhost:3000/iframe.html?selectedKind=CSS%20component&selectedStory=gutters')
+      .waitForElementPresent('.z-grid', 1000)
+      .getLocation('.z-grid', (gridLocation) => {
+        browser.assert.equal(gridLocation.value.x, -8);
+      })
+      .end();
+  },
+  reverse: (browser) => {
+    browser
+      .url('http://localhost:3000/iframe.html?selectedKind=CSS%20component&selectedStory=reverse%20grid')
+      .waitForElementPresent('.z-grid', 1000)
+      .getLocation('.z-grid', (gridLocation) => {
+        const gridX = gridLocation.value.x;
+        browser.getElementSize('.z-grid', (gridSize) => {
+          const gridWidth = gridSize.value.width;
+          browser.getElementSize('.z-grid__col:nth-child(2)', (columnSize) => {
+            const columnWidth = columnSize.value.width;
+            browser.getLocation('.z-grid__col:nth-child(2)', (columnLocation) => {
+              const expectedX = (columnLocation.value.x + columnWidth) - gridX;
+              browser.assert.equal(expectedX, gridWidth);
+            });
+          });
+        });
+      })
+      .end();
+  },
+  center: (browser) => {
+    browser
+      .url('http://localhost:3000/iframe.html?selectedKind=CSS%20component&selectedStory=center%20grid')
+      .waitForElementPresent('.z-grid', 1000)
+      .getLocation('.z-grid', (gridLocation) => {
+        const gridX = gridLocation.value.x;
+        browser.getElementSize('.z-grid', (gridSize) => {
+          const gridWidth = gridSize.value.width;
+          const expectedX = Math.floor((gridWidth / 4) + gridX);
+          browser.getLocation('.z-grid__col:nth-child(1)', (columnLocation) => {
+            const columnX = Math.floor(columnLocation.value.x);
+            browser.assert.equal(expectedX, columnX);
+          });
+        });
+      })
+      .end();
+  },
+  fit: (browser) => {
+    browser
+      .url('http://localhost:3000/iframe.html?selectedKind=CSS%20component&selectedStory=fit')
+      .waitForElementPresent('.z-grid', 1000)
+      .getElementSize('.z-grid', (gridSize) => {
+        const gridWidth = gridSize.value.width;
+        const expectedWidth = Math.floor((gridWidth / 12) * 11);
+        browser.getElementSize('.z-grid__col:nth-child(2)', (columnSize) => {
+          const columnWidth = Math.floor(columnSize.value.width);
+          browser.assert.equal(expectedWidth, columnWidth);
+        });
+      })
+      .end();
+  },
+  important: (browser) => {
+    browser
+      .url('http://localhost:3000/iframe.html?selectedKind=CSS%20component&selectedStory=important%20column%20example')
+      .waitForElementPresent('.z-grid', 1000)
+      .getLocation('.z-grid', (gridLocation) => {
+        const gridX = gridLocation.value.x;
+        browser.getLocation('.z-grid__col--important', (columnLocation) => {
+          const columnX = columnLocation.value.x;
+          const expectedX = gridX;
+          browser.assert.equal(expectedX, columnX);
+        });
+      })
+      .end();
+  },
+  resizing: (browser) => {
+    browser
+      .url('http://localhost:3000/iframe.html?selectedKind=CSS%20component&selectedStory=resizing%20example')
+      .waitForElementPresent('.z-grid', 1000)
+      .resizeWindow(1024, 768)
+      .getLocation('.z-grid', (gridLocation) => {
+        const gridX = gridLocation.value.x;
+        browser.getElementSize('.z-grid', (gridSize) => {
+          const gridWidth = gridSize.value.width;
+          const expectedX = Math.floor((gridWidth / 4) + gridX);
+          browser.getLocation('.z-grid__col:nth-child(2)', (columnLocation) => {
+            const columnX = Math.floor(columnLocation.value.x);
+            browser.assert.equal(expectedX, columnX);
+          });
+        });
+      })
+      .resizeWindow(600, 768)
+      .getLocation('.z-grid', (gridLocation) => {
+        const gridX = gridLocation.value.x;
+        browser.getElementSize('.z-grid', (gridSize) => {
+          const gridWidth = gridSize.value.width;
+          const expectedX = Math.floor((gridWidth / 2) + gridX);
+          browser.getLocation('.z-grid__col:nth-child(2)', (columnLocation) => {
+            const columnX = Math.floor(columnLocation.value.x);
+            browser.assert.equal(expectedX, columnX);
+          });
+        });
+      })
+      .resizeWindow(400, 768)
+      .getLocation('.z-grid', (gridLocation) => {
+        const gridX = gridLocation.value.x;
+        browser.getLocation('.z-grid__col:nth-child(2)', (columnLocation) => {
+          const columnX = Math.floor(columnLocation.value.x);
+          browser.assert.equal(gridX, columnX);
+        });
+      })
+      .end();
+  },
 };
--- test/unit/suite/index.js
+++ test/unit/suite/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { renderJSX, JSX } from 'jsx-test-helpers';
 import { ZGrid, ZColumn } from '../../../src/component';
 
+const NUMBER_OF_COLUMNS = 12;
+
 const Grid = ZGrid(React.createElement);
 const Column = ZColumn(React.createElement);
 
@@ -15,11 +17,165 @@ test('Grid component', (t) => {
   return t.deepEqual(actual, expected, msg);
 });
 
+test('Grid custom element', (t) => {
+  const msg = 'should render other HTML tags';
+  const actual = renderJSX(<Grid element={'nav'}>Hello!</Grid>);
+  const expected = JSX(
+    <nav className="z-grid">Hello!</nav>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Grid gutters', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Grid gutter>Hello!</Grid>);
+  const expected = JSX(
+    <div className="z-grid z-grid--gutter">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Grid center', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Grid center>Hello!</Grid>);
+  const expected = JSX(
+    <div className="z-grid z-grid--center">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Grid reverse', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Grid reverse>Hello!</Grid>);
+  const expected = JSX(
+    <div className="z-grid z-grid--reverse">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Grid vertical', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Grid vertical>Hello!</Grid>);
+  const expected = JSX(
+    <div className="z-grid z-grid--vertical">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Grid cross center', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Grid crossCenter>Hello!</Grid>);
+  const expected = JSX(
+    <div className="z-grid z-grid--cross-center">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Grid stretch', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Grid stretch>Hello!</Grid>);
+  const expected = JSX(
+    <div className="z-grid z-grid--stretch">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
 test('Column component', (t) => {
   const msg = 'should render';
   const actual = renderJSX(<Column>Hello!</Column>);
   const expected = JSX(
     <div className="z-grid__col">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Column units (normal)', (t) => {
+  const msg = 'should render';
+  for (let i = 1; i <= NUMBER_OF_COLUMNS; i += 1) {
+    const actual = renderJSX(<Column fill={i}>Hello!</Column>);
+    const expected = JSX(
+      <div className={`z-grid__col z-grid__col--${i}-12`}>Hello!</div>
+    );
+    t.deepEqual(actual, expected, msg);
+  }
+});
+
+test('Column units (sm)', (t) => {
+  const msg = 'should render';
+  for (let i = 1; i <= NUMBER_OF_COLUMNS; i += 1) {
+    const actual = renderJSX(<Column sm={i}>Hello!</Column>);
+    const expected = JSX(
+      <div className={`z-grid__col z-grid__col--sm-${i}-12`}>Hello!</div>
+    );
+    t.deepEqual(actual, expected, msg);
+  }
+});
+
+test('Column units (md)', (t) => {
+  const msg = 'should render';
+  for (let i = 1; i <= NUMBER_OF_COLUMNS; i += 1) {
+    const actual = renderJSX(<Column md={i}>Hello!</Column>);
+    const expected = JSX(
+      <div className={`z-grid__col z-grid__col--md-${i}-12`}>Hello!</div>
+    );
+    t.deepEqual(actual, expected, msg);
+  }
+});
+
+test('Column units (lg)', (t) => {
+  const msg = 'should render';
+  for (let i = 1; i <= NUMBER_OF_COLUMNS; i += 1) {
+    const actual = renderJSX(<Column lg={i}>Hello!</Column>);
+    const expected = JSX(
+      <div className={`z-grid__col z-grid__col--lg-${i}-12`}>Hello!</div>
+    );
+    t.deepEqual(actual, expected, msg);
+  }
+});
+
+test('Column units (xl)', (t) => {
+  const msg = 'should render';
+  for (let i = 1; i <= NUMBER_OF_COLUMNS; i += 1) {
+    const actual = renderJSX(<Column xl={i}>Hello!</Column>);
+    const expected = JSX(
+      <div className={`z-grid__col z-grid__col--xl-${i}-12`}>Hello!</div>
+    );
+    t.deepEqual(actual, expected, msg);
+  }
+});
+
+test('Column fit', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Column fit>Hello!</Column>);
+  const expected = JSX(
+    <div className="z-grid__col z-grid__col--fit">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Column center', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Column center>Hello!</Column>);
+  const expected = JSX(
+    <div className="z-grid__col z-grid__col--center">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Column end', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Column end>Hello!</Column>);
+  const expected = JSX(
+    <div className="z-grid__col z-grid__col--end">Hello!</div>
+  );
+  return t.deepEqual(actual, expected, msg);
+});
+
+test('Column important', (t) => {
+  const msg = 'should render';
+  const actual = renderJSX(<Column important>Hello!</Column>);
+  const expected = JSX(
+    <div className="z-grid__col z-grid__col--important">Hello!</div>
   );
   return t.deepEqual(actual, expected, msg);
 });
