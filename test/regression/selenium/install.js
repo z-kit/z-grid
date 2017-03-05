a3ff92f9012feb2c7972e4d1ea8dcaa04fdfe703
Matheus Kautzmann <kautzmann5@gmail.com>
2017-03-05 19:45:21 -0300

    Migrate from nightwatch -> nightmare

--- test/regression/selenium/install.js
+++
@@ -1,5 +0,0 @@
-const selenium = require('selenium-standalone');
-const seleniumConfig = require('./config');
-
-// Grab selenium for local visual regression testing
-module.exports = cb => selenium.install(seleniumConfig, cb);
--- test/regression/suite/component.js
+++ test/regression/suite/component.js
@@ -1,109 +1,138 @@
-module.exports = {
-  default: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=default')
-      .waitForElementPresent('.z-grid', 1000)
-      .assert.containsText('.z-grid__col--12-12 > h1', '12-12')
-      .end();
-  },
-  gutter: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=gutters')
-      .waitForElementPresent('.z-grid', 1000)
-      .getLocation('.z-grid', (gridLocation) => {
-        browser.assert.equal(gridLocation.value.x, -8);
-      })
-      .end();
-  },
-  reverse: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=reverse%20grid')
-      .waitForElementPresent('.z-grid', 1000)
-      .getLocation('.z-grid', (gridLocation) => {
-        const gridX = gridLocation.value.x;
-        browser.getElementSize('.z-grid', (gridSize) => {
-          const gridWidth = gridSize.value.width;
-          browser.getElementSize('.z-grid__col:nth-child(2)', (columnSize) => {
-            const columnWidth = columnSize.value.width;
-            browser.getLocation('.z-grid__col:nth-child(2)', (columnLocation) => {
-              const expectedX = (columnLocation.value.x + columnWidth) - gridX;
-              browser.assert.equal(expectedX, gridWidth);
-            });
-          });
-        });
-      })
-      .end();
-  },
-  center: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=center%20grid')
-      .waitForElementPresent('.z-grid', 1000)
-      .getLocation('.z-grid', (gridLocation) => {
-        const gridX = gridLocation.value.x;
-        browser.getElementSize('.z-grid', (gridSize) => {
-          const gridWidth = gridSize.value.width;
-          const expectedX = Math.floor((gridWidth / 4) + gridX);
-          browser.getLocation('.z-grid__col:nth-child(1)', (columnLocation) => {
-            const columnX = Math.floor(columnLocation.value.x);
-            browser.assert.equal(expectedX, columnX);
-          });
-        });
-      })
-      .end();
-  },
-  fit: (browser) => {
-    browser
-      .resizeWindow(640, 480)
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=fit')
-      .waitForElementPresent('.z-grid', 1000)
-      .getElementSize('.z-grid', (gridSize) => {
-        const gridWidth = gridSize.value.width;
-        const expectedWidth = Math.floor((gridWidth / 12) * 11);
-        browser.getElementSize('.z-grid__col:nth-child(2)', (columnSize) => {
-          const columnWidth = Math.floor(columnSize.value.width);
-          browser.assert.equal(expectedWidth, columnWidth);
-        });
-      })
-      .end();
-  },
-  important: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=important%20column%20example')
-      .waitForElementPresent('.z-grid', 1000)
-      .getLocation('.z-grid', (gridLocation) => {
-        const gridX = gridLocation.value.x;
-        browser.getLocation('.z-grid__col--important', (columnLocation) => {
-          const columnX = columnLocation.value.x;
-          const expectedX = gridX;
-          browser.assert.equal(expectedX, columnX);
-        });
-      })
-      .end();
-  },
-  resizing: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=resizing%20example')
-      .waitForElementPresent('.z-grid', 1000)
-      .resizeWindow(600, 768)
-      .getLocation('.z-grid', (gridLocation) => {
-        const gridX = gridLocation.value.x;
-        browser.getElementSize('.z-grid', (gridSize) => {
-          const gridWidth = gridSize.value.width;
-          const expectedX = Math.floor((gridWidth / 2) + gridX);
-          browser.getLocation('.z-grid__col:nth-child(2)', (columnLocation) => {
-            const columnX = Math.floor(columnLocation.value.x);
-            browser.assert.equal(expectedX, columnX);
-          });
-        });
-      })
-      .resizeWindow(400, 768)
-      .getLocation('.z-grid', (gridLocation) => {
-        const gridX = gridLocation.value.x;
-        browser.getLocation('.z-grid__col:nth-child(2)', (columnLocation) => {
-          const columnX = Math.floor(columnLocation.value.x);
-          browser.assert.equal(gridX, columnX);
-        });
-      })
-      .end();
-  },
-};
+import { test } from 'ava';
+import Nightmare from 'nightmare';
+
+test('SFC component - default', (t) => {
+  t.plan(1);
+  const msg = 'should render column content';
+  const expected = '12-12';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=default')
+    .wait('.z-grid')
+    .evaluate(() => document.querySelector('.z-grid__col--12-12 > h1').textContent)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - gutters', (t) => {
+  t.plan(1);
+  const msg = 'should render column gutters';
+  const expected = -8;
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=gutters')
+    .wait('.z-grid')
+    .evaluate(() => document.querySelector('.z-grid').getBoundingClientRect().left)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - reverse grid', (t) => {
+  t.plan(1);
+  const msg = 'should render a reverse grid column';
+  const expected = true;
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=reverse%20grid')
+    .wait('.z-grid')
+    .evaluate(() => {
+      const gridRight = document.querySelector('.z-grid').getBoundingClientRect().right;
+      const columnRight = document.querySelector('.z-grid__col:nth-child(2)').getBoundingClientRect().right;
+      return gridRight === columnRight;
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - center grid', (t) => {
+  t.plan(1);
+  const msg = 'should render a center grid column';
+  const expected = true;
+  return Nightmare()
+    .viewport(640, 480)
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=center%20grid')
+    .wait('.z-grid')
+    .evaluate(() => {
+      const grid = document.querySelector('.z-grid');
+      const gridX = grid.getBoundingClientRect().left;
+      const gridWidth = parseInt(getComputedStyle(grid).width, 10);
+      const expectedX = Math.floor((gridWidth / 4) + gridX);
+      const column = document.querySelector('.z-grid__col:nth-child(1)');
+      const columnX = Math.floor(column.getBoundingClientRect().left);
+      return expectedX === columnX;
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - fit', (t) => {
+  t.plan(1);
+  const msg = 'should render a column that fits the grid viewport';
+  const expected = true;
+  return Nightmare()
+    .viewport(640, 480)
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=fit')
+    .wait('.z-grid')
+    .evaluate(() => {
+      const grid = document.querySelector('.z-grid');
+      const gridWidth = parseInt(getComputedStyle(grid).width, 10);
+      const expectedWidth = Math.floor((gridWidth / 12) * 11);
+      const column = document.querySelector('.z-grid__col:nth-child(2)');
+      const columnWidth = parseInt(getComputedStyle(column).width, 10);
+      return expectedWidth === columnWidth;
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - important column example', (t) => {
+  t.plan(1);
+  const msg = 'should render important columns first';
+  const expected = 0;
+  return Nightmare()
+    .viewport(640, 480)
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=important%20column%20example')
+    .wait('.z-grid')
+    .evaluate(() => {
+      document.body.style.margin = '0';
+      return;
+    })
+    .evaluate(() => {
+      const column = document.querySelector('.z-grid__col--important');
+      return column.getBoundingClientRect().left;
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - resizing example', (t) => {
+  t.plan(2);
+  const msg = 'should handle viewport resizing';
+  const before = Nightmare()
+    .viewport(640, 768)
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=resizing%20example')
+    .wait('.z-grid')
+    .evaluate(() => {
+      const grid = document.querySelector('.z-grid');
+      const gridX = grid.getBoundingClientRect().left;
+      const gridWidth = parseInt(getComputedStyle(grid).width, 10);
+      const expectedX = Math.floor((gridWidth / 2) + gridX);
+      const column = document.querySelector('.z-grid__col:nth-child(2)');
+      const columnX = Math.floor(column.getBoundingClientRect().left);
+      return expectedX === columnX;
+    })
+    .end()
+    .then(actual => t.deepEqual(true, actual, msg));
+  const after = Nightmare()
+    .viewport(400, 768)
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=resizing%20example')
+    .wait('.z-grid')
+    .evaluate(() => {
+      document.body.style.margin = '0';
+      return;
+    })
+    .evaluate(() => {
+      const column = document.querySelector('.z-grid__col:nth-child(2)');
+      return Math.floor(column.getBoundingClientRect().left);
+    })
+    .end()
+    .then(actual => t.deepEqual(0, actual, msg));
+  return Promise.all([before, after]);
+});
